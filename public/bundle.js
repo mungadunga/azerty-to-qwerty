1eebf66a57fea6532291964c2f338b94b2820a94
mungadunga <67319507+mungadunga@users.noreply.github.com>
2021-05-27 18:53:18 +0200

    v1.0

--- public/bundle.js
+++
@@ -1,4 +0,0 @@
-const JSX = () => (React.createElement("div", null,
-    React.createElement("p", null, "Hello World!")));
-ReactDOM.render(React.createElement(JSX, null), document.querySelector("#root"));
-//# sourceMappingURL=bundle.js.map
